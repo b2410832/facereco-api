06b75bab0de28f39291c2f2778e510bc24bb4b95
rosechen <61014464+b2410832@users.noreply.github.com>
2020-09-03 13:49:37 +0800

    connect to process.env.database_url

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ const image = require('./controllers/image.js');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'rosechen',
-    password : '',
-    database : 'facereco'
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
